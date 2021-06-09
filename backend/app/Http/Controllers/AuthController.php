5bf47461839c32f119ba0e724276d3d0aac8bad1
Laciface <csomali18@gmail.com>
2021-06-09 14:49:30 +0200

    feature: open/edit profile

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -58,4 +58,65 @@ class AuthController extends Controller
             'token' => $tokenResult,
             'name' => $user->name], 200);
     }
+
+    public function editProfile(Request $request){
+        $user_id = auth()->user()->id;
+        $image = $request->picture;
+        $userName = User::where('id', $user_id)->select('name')->get('name');
+        $fileName = $userName[0]['name'] . '.' . $image->getClientOriginalExtension();
+
+        $img = Image::make($image->getRealPath());
+        $img->resize(120, 120, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+        $img->stream();
+        Storage::disk('local')->put('public/images/'. $fileName, $img);
+
+        if($request->country == null){
+            $countryDictionary = User::where('id', $user_id)->select('country')->get('country');
+            $country = $countryDictionary[0]['country'];
+        } else {
+            $country = $request->country;
+        }
+
+        if($request->introduction == null){
+            $introductionDictionary = User::where('id', $user_id)->select('introduction')->get('introduction');
+            $introduction = $introductionDictionary[0]['introduction'];
+        } else {
+            $introduction = $request->introduction;
+        }
+
+        /*if($request->picture == null){
+            $pictureDictionary = User::where('id', $user_id)->select('picture')->get('picture');
+            $picture = $pictureDictionary[0]['picture'];
+        } else {
+            $picture = $request->picture;
+        }*/
+
+
+
+        $this->validate($request,
+            [
+                'country' => 'min:4',
+                'introduction' => 'min:4',
+                'image' => "image|mimes:jpg,png,jpeg|max:15000"
+            ],
+            [
+                'country.min' => 'The country must has at least 4 chars',
+                'introduction.min' => 'The introduction must has at least 20 chars',
+                'image.image' => 'This should be an image file',
+                'image.mimes' => 'The allowed extensions: jpg,png,jpeg',
+                'image.max' => 'The allowed file size is 15000 bite'
+            ]);
+
+        $arrayToUpdate = array('country' => $country, 'introduction' => $introduction, 'picture' => $fileName);
+
+        User::where('id', $user_id)->update($arrayToUpdate);
+        return response()->json(['message' => 'Profile updated'], 200);
+    }
+
+    public function getProfileData($id){
+        $userInfo = User::where('id', $id)->get();
+        return response()->json($userInfo, 200);
+    }
 }
